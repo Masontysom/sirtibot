3526b9171a3840e5164e416a6d2350bf5509ebe8
Meeth <93545877+Masontysom@users.noreply.github.com>
2023-12-25 17:47:03 +0530

    Delete pke directory

--- pke/supervised/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
-
-from pke.supervised.api import SupervisedLoadFile
-from pke.supervised.feature_based.kea import Kea
-from pke.supervised.feature_based.wingnus import WINGNUS
